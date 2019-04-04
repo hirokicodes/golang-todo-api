455a7dace71fc71d94102d560c07dc815f852890
Hiroki Furuichi <hirokicodes@gmail.com>
2019-04-04 21:32:27 +0900

    basic gin endpoint

--- src/main.go
+++ src/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	fmt.Println("hello world")
+
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello World",
+		})
+	})
+
+	r.Run()
 }
