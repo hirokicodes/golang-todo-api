24489d434f538be0bcd29e8b40c137d2b984815b
Hiroki Furuichi <hirokicodes@gmail.com>
2019-04-05 16:49:36 +0900

    refactored endpoint

--- src/main.go
+++ src/main.go
@@ -6,15 +6,17 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func homePage(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "Hello World",
+	})
+}
+
 func main() {
 	fmt.Println("hello world")
 
 	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Hello World",
-		})
-	})
+	r.GET("/", homePage)
 
 	r.Run()
 }
